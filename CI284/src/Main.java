29f8ad20ab7622d96869b049498a855b7e9b3df6
Lozzad <36076143+Lozzad@users.noreply.github.com>
2018-11-08 16:38:20 +0000

    the files

---
+++ CI284/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+	public static void main(String[] args) {
+		PriorityQueue queue = new PriorityQueue(11);
+		
+		//testing
+		int maxpriority = 4;
+		int minpriority = 1;
+		String owner;
+		String creator;
+		
+		for (int i = 0; i < 5; i++) {
+			//select random number between min/maxpriority
+			int randomNum = minpriority + (int)(Math.random()*((maxpriority-minpriority)+1)); 
+			System.out.println("Enter ticket creator name (3 letters)");
+			creator = BIO.getString();
+			System.out.println("Enter ticket owner name (3 letters");
+			owner = BIO.getString();
+			Ticket ticket = new Ticket(creator, owner, randomNum);
+			queue.EnQueue(ticket);
+		}
+		
+		queue.PrintQueue();
+	}
+
+}
---
+++ CI284/src/Ticket.java
@@ -0,0 +1,40 @@
+
+public class Ticket {
+	//set up the variables
+	private static int numberOfTickets = 0;
+	
+	private int priority;
+	private int uniqueID;
+	private String owner;
+	private String creator;
+	
+	
+	//make the ticket object after being given a priority
+	public Ticket(String creator, String owner, int newPriority) {
+		numberOfTickets++;
+		this.uniqueID = numberOfTickets;
+		this.creator = creator;
+		this.owner = owner;
+		this.priority = newPriority;
+		
+	}
+	
+	//getters and setters (if needed)
+	public int GetTicketID() {
+		return uniqueID;
+	}
+	
+	public int GetPriority() {
+		return priority;
+	}
+	
+	public String GetOwner() {
+		return owner;
+	}
+	
+	public String GetCreator() {
+		return creator;
+	}
+	
+	
+}
